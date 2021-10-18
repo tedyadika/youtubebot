52c11b9fa0a862a9c0eac32ba9b1fa83c30dfc11
tedyadika <66624675+tedyadika@users.noreply.github.com>
2021-10-18 09:25:51 +0200

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -5,16 +5,7 @@ import random
 driver = webdriver.Chrome(r"C:\Users\New User\Downloads\chromedriver_win32 (2)\chromedriver.exe")
 driver1 = webdriver.Firefox(r'C:\Users\New User\Downloads\geckodriver-v0.30.0-win32')
 
-videos = ['https://youtu.be/xDvixmFpooQ',
-          'https://youtu.be/WxRjf4WUFpw',
-          'https://youtu.be/jhiBTViX3V8',
-          'https://youtu.be/2QE9pX8Je1M',
-          'https://youtu.be/bpy6c8fgQwY',
-          'https://youtu.be/3kP0oVGnPkU',
-          'https://youtu.be/j9q6cF0hnbg',
-          'https://youtu.be/hSPxxByIvBE',
-          'https://youtu.be/JsJKF7h4ecg',
-          'https://youtu.be/Y48LVH8wijY']
+videos = ['put list of videos you want ']
 
 
 for i in range (10000):
