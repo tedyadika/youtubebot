31afbc3fbe6ced88ced9aa7e6e15639319558ddd
tedyadika <66624675+tedyadika@users.noreply.github.com>
2022-06-08 20:26:34 +0200

    youtube

--- youtube.py
+++ youtube.py
@@ -1,18 +1,19 @@
-import time;
+import time
 from selenium import webdriver
 import random
 
-driver = webdriver.Chrome(r"C:\Users\New User\Downloads\chromedriver_win32 (2)\chromedriver.exe")
-driver1 = webdriver.Firefox(r'C:\Users\New User\Downloads\geckodriver-v0.30.0-win32')
+driver = webdriver.Chrome(r"C:\Users\New User\OneDrive\Desktop\maichbot\chromedriver.exe")
+#driver1 = webdriver.Firefox(r'C:\Users\New User\Downloads\geckodriver-v0.30.0-win32')
 
-videos = ['put list of videos you want ']
+videos = ['https://youtu.be/sALZ1C6BlCE',
+          'https://youtu.be/K4mlwAcAe4M']
 
 
 for i in range (10000):
     print('running the video for {} time'.format(i))
-    random_video = random.randint(0, 5)
+    random_video = random.randint(0, 1)
     driver.get(videos[random_video])
-    sleep_time = random.randint(50, 100)
+    sleep_time = random.randint(150, 200)
     time.sleep(sleep_time)
     
 driver.quit()
